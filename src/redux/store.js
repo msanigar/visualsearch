791d3abd352942d3f0872786383f51e2dacc7ca4
myles <myles.sanigar@missguided.com>
2018-04-13 12:15:07 +0100

    MS: remove devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -56,8 +56,7 @@ store = createStore(
   mainReducer,
   initialState,
   compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(thunk)
   )
 );
 
